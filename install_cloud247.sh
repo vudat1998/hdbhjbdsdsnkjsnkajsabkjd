965eb4f80cfbae7d1e407b4316b93bef39dc2bdd
vudat1998 <44422264+vudat1998@users.noreply.github.com>
2025-06-01 07:46:56 +0700

    Update install_cloud247.sh

--- install_cloud247.sh
+++ install_cloud247.sh
@@ -110,7 +110,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal IP = ${IP4}. External IPv6 prefix = ${IP6}"
 
-echo "Nhập số lượng proxy muốn tạo (ví dụ: 500):"
+echo "How many proxy do you want to create?"
 read -r COUNT
 
 FIRST_PORT=10000
@@ -139,4 +139,4 @@ gen_proxy_file_for_user
 systemctl daemon-reload
 systemctl restart 3proxy
 
-echo "Cài đặt hoàn tất!"
+echo "Install Done"
