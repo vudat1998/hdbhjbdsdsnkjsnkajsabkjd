f5f68d48d688953d029e8f13bf2bc57f0098659c
vudat1998 <44422264+vudat1998@users.noreply.github.com>
2025-07-02 12:23:55 -0700

    Update install_snap_vultr_centos9.sh

--- install_snap_vultr_centos9.sh
+++ install_snap_vultr_centos9.sh
@@ -7,7 +7,7 @@
 set -e
 
 echo "==> Cài đặt dependencies với dnf"
-dnf install -y gcc make wget bsdtar zip epel-release git
+dnf install -y gcc make wget bsdtar zip epel-release git openssl-devel pam-devel
 dnf install -y iproute iptables iptables-services firewalld policycoreutils-python-utils curl
 
 if ! systemctl is-active --quiet firewalld; then
@@ -33,7 +33,7 @@ if [ ! -f /usr/local/etc/3proxy/bin/3proxy ]; then
     wget -qO- "$THIRD_URL" | bsdtar -xvf- >/dev/null
     cd 3proxy-0.9.5
     make -f Makefile.Linux
-
+    make -f Makefile.Linux allplugins
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp bin/3proxy /usr/local/etc/3proxy/bin/
     cp scripts/3proxy.service /etc/systemd/system/3proxy.service
@@ -41,9 +41,10 @@ if [ ! -f /usr/local/etc/3proxy/bin/3proxy ]; then
     sed -i 's|Environment=CONFIGFILE=/etc/3proxy/3proxy.cfg|Environment=CONFIGFILE=/usr/local/etc/3proxy/3proxy.cfg|' /etc/systemd/system/3proxy.service
     sed -i 's|ExecStart=/bin/3proxy ${CONFIGFILE}|ExecStart=/usr/local/etc/3proxy/bin/3proxy ${CONFIGFILE}|' /etc/systemd/system/3proxy.service
     sed -i 's|RestartSec=60s|RestartSec=0s|' /etc/systemd/system/3proxy.service
-
+    
     chmod +x /usr/local/etc/3proxy/bin/3proxy
     cd /home/proxy-installer
+    systemctl daemon-reexec
 else
     echo "✅ 3proxy đã được cài đặt, bỏ qua bước cài."
 fi
